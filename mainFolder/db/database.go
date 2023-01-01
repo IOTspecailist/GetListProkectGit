8d3f9a1cfdd979f02fb4fc11d65a6835f176047c
nobody <asdf@asdffasdassadsad.com>
2023-01-01 20:28:12 +0900

    data save try

--- mainFolder/db/database.go
+++ mainFolder/db/database.go
@@ -1,1 +1,53 @@
-package db
+package main
+
+import (
+	"log"
+
+	boltDB "github.com/boltdb/bolt"
+)
+
+const (
+	tableName = "StationTable"
+)
+
+var database *boltDB.DB
+
+func DatabaseOpen() *boltDB.DB {
+	databasePointer, err := boltDB.Open("StationDB", 0600, nil)
+	database = databasePointer
+	HandleErr(err)
+	err = database.Update(func(tx *boltDB.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(tableName))
+		return err
+	})
+	return database
+}
+
+func DBClose() {
+	DatabaseOpen().Close()
+}
+
+func SaveIntoStationTable(data string, byteData []byte) {
+	err := DatabaseOpen().Update(func(tx *boltDB.Tx) error { //insert
+		bucket := tx.Bucket([]byte(tableName))                      // into tableName
+		err := bucket.Put([]byte(data) /*key*/, byteData /*value*/) // "byteData" values data
+		return err
+	})
+	HandleErr(err)
+}
+
+func SearchStationTable() []byte {
+	var data []byte
+	DatabaseOpen().View(func(tx *boltDB.Tx) error {
+		bucket := tx.Bucket([]byte(tableName))
+		data = bucket.Get([]byte("GangNamStation"))
+		return nil
+	})
+	return data
+}
+
+func HandleErr(err error) {
+	if err != nil {
+		log.Panic()
+	}
+}
--- mainFolder/main.go
+++ mainFolder/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"GetListProject/db"
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
 
 func main() {
 	fmt.Println("asdf")
+
+	db.SaveIntoStationTable("asdf", Tobytes("asds"))
+}
+
+func Tobytes(i interface{}) []byte {
+	var aBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&aBuffer)
+	HandleErr(encoder.Encode(i))
+	return aBuffer.Bytes()
+}
+
+func HandleErr(err error) {
+	if err != nil {
+		log.Panic()
+	}
 }
