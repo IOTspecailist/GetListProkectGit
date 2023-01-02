dfed7573d386d7aefafb4a81da84e96404520fc2
nobody <asdf@asdffasdassadsad.com>
2023-01-02 21:00:07 +0900

    패키지 임포트 에러때문에 구조수정

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
+/*
+1. hard로 데이터 insert,
+2. 함수 호출로 콘솔에 전체 출력
+
+*/
 import (
-	"GetListProject/db"
+	db "GetListProject/db" //database.go의 package를 db로 해야(맨윗줄) 패키지로 인식해서 임포트 가능
 	"bytes"
 	"encoding/gob"
 	"fmt"
-	"log"
 )
 
 func main() {
@@ -17,12 +21,6 @@ func main() {
 func Tobytes(i interface{}) []byte {
 	var aBuffer bytes.Buffer
 	encoder := gob.NewEncoder(&aBuffer)
-	HandleErr(encoder.Encode(i))
+	db.HandleErr(encoder.Encode(i))
 	return aBuffer.Bytes()
-}
-
-func HandleErr(err error) {
-	if err != nil {
-		log.Panic()
-	}
 }
