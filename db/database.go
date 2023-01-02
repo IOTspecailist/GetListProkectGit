a64ce369f55457a7a89c2987b1bb11e95ecd930a
nobody <asdf@asdffasdassadsad.com>
2023-01-02 21:26:38 +0900

    insert done

--- db/database.go
+++ db/database.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"bytes"
+	"encoding/gob"
 	"log"
 
 	boltDB "github.com/boltdb/bolt"
@@ -17,6 +19,7 @@ func DatabaseOpen() *boltDB.DB {
 	database = databasePointer
 	HandleErr(err)
 	err = database.Update(func(tx *boltDB.Tx) error {
+		HandleErr(err)
 		_, err := tx.CreateBucketIfNotExists([]byte(tableName))
 		return err
 	})
@@ -27,10 +30,35 @@ func DBClose() {
 	DatabaseOpen().Close()
 }
 
+type Station struct {
+	StationName string
+	Dosi        string
+}
+
+func CreateStation() *Station {
+	station := &Station{
+		StationName: "GangNam",
+		Dosi:        "Seoul",
+	}
+	station.AddStation()
+	return station
+
+}
+func (s *Station) AddStation() {
+	SaveIntoStationTable(s.StationName, Tobytes(s))
+}
+
+func Tobytes(i interface{}) []byte {
+	var aBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&aBuffer)
+	HandleErr(encoder.Encode(i))
+	return aBuffer.Bytes()
+}
+
 func SaveIntoStationTable(data string, byteData []byte) {
 	err := DatabaseOpen().Update(func(tx *boltDB.Tx) error { //insert
-		bucket := tx.Bucket([]byte(tableName))                      // into tableName
-		err := bucket.Put([]byte(data) /*key*/, byteData /*value*/) // "byteData" values data
+		bucket := tx.Bucket([]byte(tableName))                        // into tableName
+		err := bucket.Put([]byte(data) /*=key*/, byteData /*=value*/) // "byteData" values data
 		return err
 	})
 	HandleErr(err)
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-1. hard로 데이터 insert,
+1. hard로 데이터 insert
 2. 함수 호출로 콘솔에 전체 출력
 
 */
@@ -13,12 +13,15 @@ import (
 )
 
 func main() {
-	fmt.Println("asdf")
 
-	db.SaveIntoStationTable("asdf", Tobytes("asds"))
+	defer db.DBClose()
+	fmt.Println(db.CreateStation())
+
+	//db.SaveIntoStationTable("asdf", Tobytes("asds"))
 }
 
 func Tobytes(i interface{}) []byte {
+
 	var aBuffer bytes.Buffer
 	encoder := gob.NewEncoder(&aBuffer)
 	db.HandleErr(encoder.Encode(i))
