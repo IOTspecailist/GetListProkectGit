a66cd065d698bc07f16e2df0e435f9bef11cfdc3
nobody <asdf@asdffasdassadsad.com>
2023-01-03 22:10:09 +0900

    []byte연습완

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 /*
-1. hard로 데이터 insert
-2. 함수 호출로 콘솔에 전체 출력
+1. []byte 타입으로 데이터 출력하기 --완
+	바이트배열 선언 및 사용법
+	바이트, 비트, 스트링 개념 구분
+	변수 선언방식
+	한글을 표현하는 char-set 정리했음 헷갈려서
+	구글드라이브에
+2. []byte 타입을 받는 함수에 데이터 전달하기 --완
+3. []byte 타입 데이터 읽어서 string 출력 하는 원리(함수분석 -이건안함 고언어가 기본제공하는 함수 이용해서 만든거라)
+4. 소스 한줄씩 분석
+5. 리팩토링
+
 
 */
 import (
@@ -23,7 +32,9 @@ func main() {
 	var asdf = BytesToString(db.SearchStationTable())
 	fmt.Println(asdf)
 	//db.SaveIntoStationTable("asdf", Tobytes("asds"))
+
 }
+
 func BytesToString(b []byte) string {
 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	sh := reflect.StringHeader{bh.Data, bh.Len}
