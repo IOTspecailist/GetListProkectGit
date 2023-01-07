9740319b941f8eea3fe58c68c763e332874fd9b8
nobody <asdf@asdffasdassadsad.com>
2023-01-07 17:49:17 +0900

    station에서 mma로 변환

--- db/database.go
+++ db/database.go
@@ -10,15 +10,16 @@ import (
 )
 
 const (
-	tableName = "StationTable"
+	DataBaseName = "MMACompanyDB"
+	TableName    = "MMACompanyTable"
 )
 
 var database *boltDB.DB
 var bucc *boltDB.Bucket
 
 func DatabaseOpen() *boltDB.DB {
-	databasePointer, err := boltDB.Open("StationDB", 0600, nil)
-	// nil을 줌으로써 디폴트옵션으로 StationDB란 이름의 디비를 오픈함
+	databasePointer, err := boltDB.Open(DataBaseName, 0600, nil)
+	// nil을 줌으로써 디폴트옵션으로  MMACompanyDB 이름의 디비를 오픈함
 	// 만약 처음이라면 파일을 생성한다
 	// 쉽게말해서 가방을 하나 사서 이름을 붙여준거다 / 가방 = 데이터베이스, 가방에 넣는 물건 = 데이터
 	// 볼트디비의 포인터 구조체를 리턴받는다 / 에러리턴은 왜그런건지
@@ -26,7 +27,7 @@ func DatabaseOpen() *boltDB.DB {
 	HandleErr(err)
 	err = database.Update(func(tx *boltDB.Tx) error {
 		HandleErr(err)
-		bucc, err := tx.CreateBucketIfNotExists([]byte(tableName))        // StationTable이란 이름의 테이블이 없으면 생성한다
+		bucc, err := tx.CreateBucketIfNotExists([]byte(TableName))        // StationTable이란 이름의 테이블이 없으면 생성한다
 		fmt.Println("DatabaseOpen() :: returned *boltDB.Bucket : ", bucc) // 언더바_로 리턴값을 버리고 있길래 변수선언해서 받음
 		return err
 	})
@@ -41,26 +42,26 @@ func DBClose() {
 	//func (db *DB) Close() error {
 }
 
-type Station struct {
-	StationName string
-	Dosi        string
+//팀 정보 기본 틀
+type Team struct {
+	TeamName string
+	Player   []string
 }
 
-func CreateStation() *Station {
-	station := &Station{
-		StationName: "GangNam",
-		Dosi:        "Seoul",
+//팀 만들기(팀이름 하나에 플레이어 여러명)
+func CreateTeam(teamName string, player []string) *Team {
+	team := &Team{
+		TeamName: teamName,
+		Player:   player,
 	}
-	station.AddStation()
-	return station
-
+	team.Teaminfo_Into_BoltDB()
+	return team
 }
 
-func (s *Station) Restore(data []byte) {
-	FromBytes(s, data)
-}
-func (s *Station) AddStation() {
-	SaveIntoStationTable(s.StationName, Tobytes(s))
+//insert문 호출하는 리시버펑션, 리시버펑션은 struct의 값에 변화를 준다
+func (s *Team) Teaminfo_Into_BoltDB() {
+
+	Insert_Into_MMACompanyTable(s.TeamName, Tobytes(s))
 }
 
 func Tobytes(i interface{}) []byte {
@@ -70,9 +71,10 @@ func Tobytes(i interface{}) []byte {
 	return aBuffer.Bytes()
 }
 
-func SaveIntoStationTable(data string, byteData []byte) {
+//insert into DB
+func Insert_Into_MMACompanyTable(data string, byteData []byte) {
 	err := DatabaseOpen().Update(func(tx *boltDB.Tx) error { //insert
-		bucket := tx.Bucket([]byte(tableName))                        // into tableName
+		bucket := tx.Bucket([]byte(TableName))                        // into tableName
 		err := bucket.Put([]byte(data) /*=key*/, byteData /*=value*/) // "byteData" values data
 		return err
 	})
@@ -83,7 +85,7 @@ func SaveIntoStationTable(data string, byteData []byte) {
 func SearchStationTable() []byte {
 	var data []byte                                 //볼트디비는 인아웃이 바이트배열이므로 디비조회해서 결과를 저장할 변수를 선언함 / 리턴도 할겸
 	DatabaseOpen().View(func(tx *boltDB.Tx) error { //select value
-		bucket := tx.Bucket([]byte(tableName)) //from tableName
+		bucket := tx.Bucket([]byte(TableName)) //from tableName
 		data = bucket.Get([]byte("GangNam"))   // where key = GangNam
 		return nil
 	})
@@ -101,4 +103,8 @@ func FromBytes(i interface{}, data []byte) {
 	encoder := gob.NewDecoder(bytes.NewReader(data))
 	HandleErr(encoder.Decode(i))
 	//포인터로 복원해주는 거다? i로 온게 포인터라 그런가 그래서 리턴이 없나
+}
+
+func (s *Team) Restore(data []byte) {
+	FromBytes(s, data)
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 /*
-
-1. 소스 한줄씩 분석
-2. 리팩토링
+20230107
+역말고 mma로 재설계
+1. 데이터베이스1개, 이름은 MMACompanyDB
+2. 테이블 1개, 이름은 MMACompanyTable
+3. 테이블은 키, 값 구조이며 키는 단체명, 값은 선수명
 
 
 */
@@ -11,20 +13,35 @@ import (
 	db "GetListProject/db" //database.go의 package를 db로 해야(맨윗줄) 패키지로 인식해서 임포트 가능
 	"bytes"
 	"encoding/gob"
-	"fmt"
 	"reflect"
 	"unsafe"
 )
 
+const (
+	Team1 = "TeamUFC"
+	Team2 = "TeamRoadFC"
+)
+
+var TeamPlayer []string
+
 func main() {
 
 	defer db.DBClose()
-	// //fmt.Println(db.CreateStation())
-	// //db.CreateStation().Restore(db.SearchStationTable())
-	// //fmt.Println(db.CreateStation())
-	var asdf = BytesToString(db.SearchStationTable())
-	fmt.Println(asdf)
-	// //db.SaveIntoStationTable("asdf", Tobytes("asds"))
+	TeamPlayer[0] = "enganu"
+	TeamPlayer[1] = "colvi"
+	TeamPlayer[2] = "JungChanSung"
+	db.CreateTeam(Team1, TeamPlayer)
+
+	TeamPlayer[0] = "MHMan"
+	TeamPlayer[1] = "HISu"
+	TeamPlayer[2] = "YYJun"
+	db.CreateTeam(Team2, TeamPlayer)
+
+	//db.CreateTeam().Restore(db.SearchStationTable())
+	//fmt.Println(db.CreateTeam())
+	//var asdf = BytesToString(db.SearchStationTable())
+	//fmt.Println(asdf)
+	//db.Insert_Into_MMACompanyTable("asdf", Tobytes("asds"))
 }
 
 func BytesToString(b []byte) string {
@@ -32,22 +49,10 @@ func BytesToString(b []byte) string {
 	sh := reflect.StringHeader{bh.Data, bh.Len}
 	return *(*string)(unsafe.Pointer(&sh))
 }
+
 func Tobytes(i interface{}) []byte { // 어떤 값이든 받을 수 있도록 매개변수 interface로 선언
 	var aBuffer bytes.Buffer            // 바이트배열로 만들기위한 틀(aBuffer)
 	encoder := gob.NewEncoder(&aBuffer) // 틀을 가지고 재료를 굽는 오븐(encoder) 구입 (틀이 들어가 있는 오븐임)
 	db.HandleErr(encoder.Encode(i))     // 오븐(encoder)안에 틀(aBuffer)이 있는상황에서 재료(i)를 투입하여 오븐(encoder)의 굽기버튼 클릭(Encode())
 	return aBuffer.Bytes()              // 틀을 꺼내서 식탁으로 틀(aBuffer)에있는 빵(Bytes())을 리턴 // Bytes()는 bytes 패키지의 Buffer struct의 리시버 펑션이다
 }
-
-/*
-1. 값이 i로 들어옴
-2. i를 []byte(바이트배열)로 만들기 위해 bytes 패키지 사용
-3. bytes 패키지를 encoding 하기 위해
-3. aBuffer 변수에 bytes 패키지의 Buffer Struct를 선언 (초기화 아직안함)
-4. encoder 변수를 encoding/gob 패키지의 NewEncoder()로 선언과 동시에 초기화
-	NewEncoder()는 io패키지의 Write interface를 매개변수로 받고, *Encoder struct를 리턴함
-	NewEncoder()의 매개변수타입이 interface이므로  &aBuffer를 매개변수로 받는게 가능하고
-	encoder 변수의 타입은 *gob.Encoder struct로 정의됨
-5. Encode()는 gob.Encoder struct의 리시버 펑션이고 매개변수를 interface 타입으로 받음
-아 이렇게 하면 끝이 없네
-*/
